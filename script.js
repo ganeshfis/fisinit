8b5845f43f269be18ad0fd9cb3e7cec37d277989
Ganesh <ganes.k@fisdom.com>
2022-07-15 12:09:15 +0530

    again code input

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("i can drive");
+*/
+
+function logger() {
+    console.log("my name is Ganesh");
+}
+//function call
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples
+    and ${oranges} oranges`;
+    return juice;
+
+}
+
+const aJ = fruitProcessor(4, 6);
+console.log(aJ);
+
+const appleOrangeJuice = fruitProcessor(3, 5);
+console.log(appleOrangeJuice);
